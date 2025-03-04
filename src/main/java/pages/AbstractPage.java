d1d2f075fb70c6ef0b1f2a164902e82250735f50
Tuan Le <hoangtuan.ln97@gmail.com>
2025-03-04 17:14:23 +0700

    add Constant.java and Utilities.java

--- src/main/java/pages/AbstractPage.java
+++ src/main/java/pages/AbstractPage.java
@@ -1,18 +1,19 @@
 package pages;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-
 public abstract class AbstractPage {
 	protected WebDriver driver;
-	
+
 	private final String linkNavigationBar = "//a[@class='nav-link'][normalize-space(text()) = '%s']";
-	
+
 	public AbstractPage(WebDriver driver) {
 		this.driver = driver;
-		System.out.println("WebDriver is on Thread: " + Thread.currentThread().getId() + "---> " + this.driver);
+		System.out.println(this.getClass().getName() + " - WebDriver is on Thread: " + Thread.currentThread().getId()
+				+ "---> " + this.driver);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public <T extends AbstractPage> T clickOnNavigationBar(final String name) {
 		String locator = String.format(linkNavigationBar, name);
